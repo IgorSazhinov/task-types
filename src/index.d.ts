d990c818c35381b95a63bc787a1656d753ca50da
ia.sazhinov@tensor.ru <ia.sazhinov@tensor.ru>
2025-04-29 09:31:54 +0300

    тест

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,31 +1,20 @@
-type User = {
+export function getPersons(): {
   name: string;
   age: number;
   gender: 'male' | 'female';
-};
+}[];
 
-type Employee = {
-  company: string;
-} & User;
-
-/*
-Типизируйте функцию getPersons, возвращающую массив объектов со следующими свойствами:
-- name - строка
-- age - число
-- gender - 'male' или 'female'
- */
-export function getPersons(): User[];
-
-/*
-Типизируйте функцию personToString, возвращающую строку и принимающую объект одного из следующих форматов:
-   1. Пользователь:
-   - name - строка
-   - age - число
-   - gender - 'male' или 'female'
-   2. Сотрудник
-   - name - строка
-   - age - число
-   - gender - 'male' или 'female'
-   - company- строка
- */
-   export function personToString(person: User | Employee): string;
+export function personToString(
+  person:
+      | {
+            name: string;
+            age: number;
+            gender: 'male' | 'female';
+        }
+      | {
+            name: string;
+            age: number;
+            gender: 'male' | 'female';
+            company: string;
+        },
+): string;
